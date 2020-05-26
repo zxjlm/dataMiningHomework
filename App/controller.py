d988367fac202d04c7c474517f9cf316a388a3ee
harumonia <zxjlm233@gmail.com>
2020-05-26 11:35:09 +0800

    add

--- App/controller.py
+++ App/controller.py
@@ -1,10 +1,11 @@
 from brewer2mpl import brewer2mpl
+from flask import g
 
 from App.models import Diagnosis, HerbInfo
 
 
 def get_all_pres():
-    return Diagnosis.query.with_entities(Diagnosis.id, Diagnosis.prescription).all()
+    return Diagnosis.query.with_entities(Diagnosis.id, Diagnosis.province, Diagnosis.prescription).all()
 
 
 def get_feature(ll_xing, ll_wei, ll_guijing, ll_query_condition):
@@ -19,7 +20,7 @@ def get_feature(ll_xing, ll_wei, ll_guijing, ll_query_condition):
         }
         legend_data_list.append(str(diagnosis.id) + diagnosis.province)
         herb_list = diagnosis.prescription.split(';')
-        feature_counter_res = feature_counter(object_dic, herb_list)
+        feature_counter_res, _ = feature_counter(object_dic, herb_list, False)
 
         xing_result_list.append(list(feature_counter_res['xing'].values()))
         wei_result_list.append(list(feature_counter_res['wei'].values()))
@@ -27,7 +28,8 @@ def get_feature(ll_xing, ll_wei, ll_guijing, ll_query_condition):
     return [xing_result_list, wei_result_list, guijing_result_list], legend_data_list
 
 
-def feature_counter(object_dic, ll_herb):
+def feature_counter(object_dic, ll_herb, stat=True):
+    unfind = []
     for herb in ll_herb:
         herb_info = HerbInfo.query.filter(HerbInfo.name == herb).first()
         if herb_info:
@@ -35,10 +37,13 @@ def feature_counter(object_dic, ll_herb):
                 obj_feature_res = getattr(herb_info, obj)
                 if obj_feature_res:
                     for feature in getattr(herb_info, obj).split('、'):
-                        object_dic[obj][feature] += 1
+                        if stat:
+                            object_dic[obj][feature] += 1 / len(ll_herb)
+                        else:
+                            object_dic[obj][feature] += 1
         else:
-            continue
-    return object_dic
+            unfind.append(herb)
+    return object_dic, unfind
 
 
 def get_feature_for_each_dig(ll_xing, ll_wei, ll_guijing):
@@ -53,18 +58,38 @@ def get_feature_for_each_dig(ll_xing, ll_wei, ll_guijing):
         }
         legend_data_list.append(str(diagnosis.id) + diagnosis.province)
         herb_list = diagnosis.prescription.split(';')
-        feature_counter_res = feature_counter(object_dic, herb_list)
+        feature_counter_res, _ = feature_counter(object_dic, herb_list)
         diagnosis_res_list.append(
             list(feature_counter_res['xing'].values()) + list(feature_counter_res['wei'].values()) +
             list(feature_counter_res['guijing'].values()))
     return diagnosis_res_list, legend_data_list
 
 
+def classif_heandler(data, ll_xing, ll_wei, ll_guijing):
+    import pickle
+    object_dic = {
+        'xing': dict(zip(ll_xing, [0] * len(ll_xing))),
+        'wei': dict(zip(ll_wei, [0] * len(ll_wei))),
+        'guijing': dict(zip(ll_guijing, [0] * len(ll_guijing)))
+    }
+    herb_list = data.split(';')
+    feature_counter_res, unfind = feature_counter(object_dic, herb_list)
+    with open('kmeans_fit.pkl', 'rb') as file:
+        kmeans = pickle.load(file)
+    return kmeans.predict([list(feature_counter_res['xing'].values()) + list(feature_counter_res['wei'].values()) +
+                           list(feature_counter_res['guijing'].values())]), kmeans.labels_, unfind
+
+
 def km_cul(data, legend_list, clusters_num=7):
     from sklearn.cluster import KMeans
+    import pickle
+    labels_dict = {}
     bmap = brewer2mpl.get_map('Set3', 'Qualitative', 12)
-    kmeans = KMeans(n_clusters=clusters_num, random_state=0).fit(data)
-    labels = kmeans.labels_
+    kmeans = KMeans(n_clusters=clusters_num, random_state=0)
+    kmeans_fit = kmeans.fit(data)
+    labels = kmeans_fit.labels_
+    with open('kmeans_fit.pkl', 'wb') as file:
+        pickle.dump(kmeans, file)
     # legend_dict = [{v: labels[k]} for k, v in enumerate(legend_list)]
     ind_cluster_list, links, nodes = [], [], [{'name': foo} for foo in legend_list]
     for ind in range(clusters_num):
@@ -74,10 +99,20 @@ def km_cul(data, legend_list, clusters_num=7):
                 continue
 
     for ind, label in enumerate(labels):
+        if label in list(labels_dict.keys()):
+            labels_dict[label].append(ind)
+        else:
+            labels_dict[label] = [ind]
         if legend_list[ind_cluster_list[label]] == legend_list[ind]:
             continue
         links.append({'source': legend_list[ind_cluster_list[label]], 'target': legend_list[ind],
                       'lineStyle': {
                           'color': 'rgb({},{},{})'.format(bmap.colors[label % 12][0], bmap.colors[label % 12][1],
                                                           bmap.colors[label % 12][2])}})
-    return nodes, links
+
+    return nodes, links, labels_dict
+
+
+def my_map(ll, num):
+    import numpy as np
+    return list(np.array(ll) / num)
--- App/route.py
+++ App/route.py
@@ -4,7 +4,7 @@ __author__ = 'xiejdm'
 from App import app, db, restless
 from flask import render_template, jsonify, request
 from App.models import HerbInfo, Diagnosis
-from App.controller import get_feature, get_all_pres, get_feature_for_each_dig, km_cul
+from App.controller import get_feature, get_all_pres, get_feature_for_each_dig, km_cul, classif_heandler
 from efficient_apriori import apriori
 import brewer2mpl
 
@@ -14,6 +14,10 @@ restless.create_api(Diagnosis, include_methods=['text'], methods=['GET', 'POST',
 
 @app.route('/')
 def index():
+    '''
+    中国疫情页面(主页)
+    :return:
+    '''
     return render_template('china.html')
 
 
@@ -21,7 +25,7 @@ def index():
 @app.route('/num_count/<string:query_condition>')
 def num_count(query_condition=None):
     '''
-
+    频次统计
     :param query_condition:查询条件
     :return:渲染参数：
     '''
@@ -41,6 +45,10 @@ def num_count(query_condition=None):
 
 @app.route('/components/rule', methods=['POST'])
 def rules_cul():
+    '''
+    计算关联规则
+    :return:
+    '''
     form = request.json
     data_raw = get_all_pres()
     # data = [data_raw[ind]['prescription'].split(';') for ind in form['data']]
@@ -71,28 +79,62 @@ def rules_cul():
                               'color': 'rgb({},{},{})'.format(bmap.colors[ind % 12][0], bmap.colors[ind % 12][1],
                                                               bmap.colors[ind % 12][2])}})
     nodes = [{'name': foo} for foo in set(nodes_tmp)]
-    print(nodes)
     return render_template('content/rulesRes.html', items=item_new, rules=rules_new, nodes=nodes, links=links)
 
 
 @app.route('/rules/')
 def rules():
+    '''
+    关联规则页面
+    :return:
+    '''
     return render_template('rules.html', pres_dic=dict(enumerate(get_all_pres())))
 
 
 @app.route('/components/km', methods=['POST'])
 def ap_km():
+    '''
+    计算聚类
+    :return:
+    '''
     data, legend = get_feature_for_each_dig(app.config['XING_LIST'], app.config['WEI_LIST'],
                                             app.config['GUIJING_LIST'])
-    nodes, links = km_cul(data, legend, int(request.json['num']))
-    return render_template('content/kmRes.html', nodes=nodes, links=links)
+    nodes, links, labels_dict = km_cul(data, legend, int(request.json['num']))
+    pres_ll = get_all_pres()
+    return render_template('content/kmRes.html', nodes=nodes, links=links, labels_dict=labels_dict,
+                           pres_dic=dict(enumerate(pres_ll)))
 
 
 @app.route('/km')
 def km():
+    '''
+    聚类页面
+    :return:
+    '''
     return render_template('km.html')
 
 
 @app.route('/china')
 def china():
+    '''
+    世界疫情页面
+    :return:
+    '''
     return render_template('index.html')
+
+
+@app.route('/classif', methods=['POST'])
+def classif():
+    '''
+
+    :return:
+    '''
+    res, lables, unfind = classif_heandler(request.get_json()['data'], app.config['XING_LIST'], app.config['WEI_LIST'],
+                                           app.config['GUIJING_LIST'])
+    pres_ll = get_all_pres()
+    res_fin = []
+    for k, v in enumerate(lables):
+        if v == res[0]:
+            res_fin.append(pres_ll[k])
+
+    return render_template('content/predict.html', res=res_fin, typ=res[0], unfind=','.join(unfind))
